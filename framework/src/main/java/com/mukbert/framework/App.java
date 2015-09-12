37d20e60a7dadbf385928ec6f260a9f7c61344e2
Dominik Fuchs <domi.fuchs@hotmail.de>
2015-09-13 01:33:12 +0200

    Created JFrame Test

---
+++ framework/src/main/java/com/mukbert/framework/App.java
@@ -0,0 +1,17 @@
+package com.mukbert.framework;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	JFrame frame = new JFrame("Hello World");
+    	frame.setDefaultCloseOperation(3);
+    	frame.setVisible(true);
+    }
+}
